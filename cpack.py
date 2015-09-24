85984bc33e935c3a1f7dfe3ab4226d11716c290b
Jakub Staron <staron.jk@gmail.com>
2015-09-24 20:55:09 +0200

    cpack: style changes.

--- cpack.py
+++ cpack.py
@@ -5,24 +5,25 @@ except ImportError:
     print("cpack.py requires ultima.py to run!")
     exit()
 
+
 def getProviderListFromArgs(args, parser):
     testProviderList = list()
     for zipFile in args.zip:
         if getFileNameExtension(zipFile) != "zip":
             parser.error("received not zip file")            
         assertFileExist(zipFile) 
-        value = (TestFromZipProvider, (zipFile,) )
+        value = (TestFromZipProvider, (zipFile,))
         testProviderList.append(value)
         
     for folder in args.folder:
         assertFolderExist(folder) 
-        value = (TestFromFolderProvider, (folder,) ) 
+        value = (TestFromFolderProvider, (folder,))
         testProviderList.append(value)
         
     for generator in args.generator:
         generatorPath = generator[0]
         assertFileExist(generatorPath)
-        value = (RandomTestProvider, (generatorPath, args.testname, args.program, "",int(generator[1]))) 
+        value = (RandomTestProvider, (generatorPath, args.testname, args.program, "", int(generator[1])))
         testProviderList.append(value)
     
     for generator in args.generator2:
@@ -31,28 +32,25 @@ def getProviderListFromArgs(args, parser):
         value = (RandomTestProvider, (generatorPath, args.testname, args.program, generator[2], int(generator[1]))) 
         testProviderList.append(value)
 
-
     return testProviderList
 
 
-
-def testingLoop(testProviderList, args):
+def mainLoop(testProviderList, args):
     runner = BasicRunner(args.program)
     runner.ignoreOutput = True
-    
+
     output = zipfile.ZipFile(args.output, 'w', zipfile.ZIP_DEFLATED, True)
 
-
-    for testProviderArgs in testProviderList:    
+    for testProviderArgs in testProviderList:
         TestProviderClass = testProviderArgs[0]
         testProviderArgs = testProviderArgs[1]
-        print("Procesing tests from \"%s\"" % (testProviderArgs,) )
+        print("Processing tests from \"%s\"" % (testProviderArgs,))
         testProvider = TestProviderClass(*testProviderArgs)
-        
+
         test_number = 0
-        for test in testProvider.getTests():    
-            runResult = runner.run(test)         
-    
+        for test in testProvider.getTests():
+            runResult = runner.run(test)
+
             if runResult.result not in ("OK", "IGNORE"):
                 print("Error when doing test %s" % test.testName)
                 if args.break_after_error:
@@ -60,15 +58,13 @@ def testingLoop(testProviderList, args):
                     tryDeleteFile(args.output)
                     return
 
-            test_number = test_number + 1
+            test_number += 1
             sys.stdout.write("\r%s tests done." % test_number)
 
             inname = "in/%s.in" % test.testName
             outname = "out/%s.out" % test.testName
-            output.writestr(inname, test.inData)
-            output.writestr(outname, runResult.outData)
-
-
+            output.writestr(inname, test.inputData)
+            output.writestr(outname, runResult.outputData)
 
 
 def main():
@@ -81,14 +77,19 @@ def main():
     sourcesGroup = parser.add_argument_group("Test sources")
     sourcesGroup.add_argument('--zip', '-z', help='zip file(s) as test source', nargs='+', default=list())
     sourcesGroup.add_argument('--folder', '-f', help='folder with tests as source', action='append', default=list())
-    sourcesGroup.add_argument('--generator', '-g', help='test generator and number of tests to create as test source', nargs=2, metavar=("GEN", "NUM"), action='append', default=list())
-    sourcesGroup.add_argument('--generator2', '-g2', help='test generator, number of tests and test name suffix (eg. test123small.in)', nargs=3, metavar=("GEN", "NUM", "SUF"), action='append', default=list())
+    sourcesGroup.add_argument('--generator', '-g', help='test generator and number of tests to create as test source',
+                              nargs=2, metavar=("GEN", "NUM"), action='append', default=list())
+    sourcesGroup.add_argument('--generator2', '-g2',
+                              help='test generator, number of tests and test name suffix (eg. test123small.in)',
+                              nargs=3, metavar=("GEN", "NUM", "SUF"), action='append', default=list())
     
-    parser.add_argument('--break_after_error', '-b', help='break pack creating after error, erase half created pack', action='store_false', default=True)
+    parser.add_argument('--break_after_error', '-b', help='break pack creating after error, erase half created pack',
+                        action='store_false', default=True)
+
+    parser.add_argument('--threads', '-t', help='number of parallel tasks', type=int, default=1)
 
     args = parser.parse_args()    
     assertFileExist(args.program)
-
 
     if getFileNameExtension(args.output) != "zip":
         args.output += ".zip"
@@ -97,9 +98,8 @@ def main():
         print("I will not overwrite existing file.")
         return
 
-
     providerList = getProviderListFromArgs(args, parser)
-    testingLoop(providerList, args)
+    mainLoop(providerList, args)
     
 
 if __name__ == "__main__":
@@ -107,4 +107,3 @@ if __name__ == "__main__":
         main()
     except KeyboardInterrupt:
         print("\nKeyboardInterrupt - Exiting...")
-
